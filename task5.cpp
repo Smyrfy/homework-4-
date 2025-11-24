d895821c8759c1bb8c9d8568febf8ea5a9142a39
Dasha <dudko.dasha72773@gmail.com>
2025-11-24 10:51:33 +0000

    ////

--- task5.cpp
+++ task5.cpp
@@ -1,20 +1,25 @@
 #include <iostream> 
+
 int main() {
   int m, n;
   std::cout << "Enter m and n" << std::endl;
   std::cin >> m >> n;
   int p = 0;
+
   for (int i = 0; i < m; i++) {
     int b = p;
     for (int j = n; j > 0; j--) {
-      if (i%2 == 0) {
+      if (i % 2 == 0) {
         std::cout << p << " ";
         p++;
-      }
-      else {
-        std::cout << b+j-1 << " ";
+      } else {
+        std::cout << b + j - 1 << " ";
         p++;
       }
     }
     std::cout << std::endl;
   }
+
+  return 0;
+}
+
