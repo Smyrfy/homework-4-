d87c85d5787d3b7d8be21764417a8e3f05de1caf
Dasha <dudko.dasha72773@gmail.com>
2025-11-24 10:45:21 +0000

    modified:   task5.cpp

--- task5.cpp
+++ task5.cpp
@@ -1,42 +1,20 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-void snakePattern(int rows, int cols) {
-    vector<vector<int>> matrix(rows, vector<int>(cols, 0));
-    
-    int num = 1;
-
-    for (int i = 0; i < rows; i++) {
-        if (i % 2 == 0) {
-            for (int j = 0; j < cols; j++) {
-                matrix[i][j] = num++;
-            }
-        } else { 
-            for (int j = cols - 1; j >= 0; j--) {
-                matrix[i][j] = num++;
-            }
-        }
-    }
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
+#include <iostream> 
 int main() {
-    int rows, cols;
-    
-    cout << "Enter number of rows: ";
-    cin >> rows;
-    cout << "Enter number of columns: ";
-    cin >> cols;
-    
-    snakePattern(rows, cols);
-    
-    return 0;
-}
-
+  int m, n;
+  std::cout << "Enter m and n" << std::endl;
+  std::cin >> m >> n;
+  int p = 0;
+  for (int i = 0; i < m; i++) {
+    int b = p;
+    for (int j = n; j > 0; j--) {
+      if (i%2 == 0) {
+        std::cout << p << " ";
+        p++;
+      }
+      else {
+        std::cout << b+j-1 << " ";
+        p++;
+      }
+    }
+    std::cout << std::endl;
+  }
