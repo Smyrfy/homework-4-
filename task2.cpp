abfa5f1274c026f00affd7c191d380317ab35fc4
Dasha <dudko.dasha72773@gmail.com>
2025-11-25 21:19:49 +0000

    //

--- task2.cpp
+++ task2.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 using namespace std;
 
+int stringLength(const char* str) {
+    int length = 0;
+    
+    while (str[length] != '\0') {
+        length++;
+    }
+    return length;
+}
+
 int main() {
-    string s;
-    cin >> s;
-    cout << s.size();
+    char str[100];  
+    
+    cout << "Enter a string: ";
+    cin >> str;  
+    
+    int length = stringLength(str); 
+    cout << "Length of the string: " << length << endl; 
+    
+    return 0;
 }
